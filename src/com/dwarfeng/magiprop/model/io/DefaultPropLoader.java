409101e1102f6c6c207ff0c04aa542f015418d4f
DwArFeng <915724865@qq.com>
2016-11-05 18:29:47 +0800

    20161105 Commit B
    Signed-off-by: DwArFeng <915724865@qq.com>

---
+++ src/com/dwarfeng/magiprop/model/io/DefaultPropLoader.java
@@ -0,0 +1,29 @@
+package com.dwarfeng.magiprop.model.io;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import com.dwarfeng.magiprop.model.struct.PropLabel;
+import com.dwarfeng.magiprop.model.struct.PropValue;
+import com.dwarfeng.magiprop.model.struct.TextPropLabel;
+
+public class DefaultPropLoader implements PropLoader {
+
+	public DefaultPropLoader() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<PropLabel> loadLabels() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PropValue loadValue(TextPropLabel textPropLabel, Locale locale) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
