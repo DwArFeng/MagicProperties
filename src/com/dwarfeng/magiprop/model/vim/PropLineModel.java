b76a9e21e3da481d9d750dac7cec004bb47f25fe
DwArFeng <915724865@qq.com>
2016-11-17 12:04:20 +0800

    20161117 Commit A
    Signed-off-by: DwArFeng <915724865@qq.com>

--- src/com/dwarfeng/magiprop/model/vim/PropLineModel.java
+++ src/com/dwarfeng/magiprop/model/vim/PropLineModel.java
@@ -13,29 +13,29 @@ import com.dwarfeng.magiprop.model.struct.PropLine;
  */
 public interface PropLineModel extends List<PropLine>{
 	
-//	/**
-//	 * 指定的属性行的键是否在所有相关的语言中都拥有。
-//	 * @param index 指定的序号对应的属性行。
-//	 * @return 对应的属性行的键是否在所有相关的语言中都拥有
-//	 * @throws IndexOutOfBoundsException 序号越界。 
-//	 */
-//	public boolean isComplete(int index);
-//	
-//	/**
-//	 * 指定的属性行的键是否在至少一个相关的语言中不拥有。
-//	 * @param index 指定的序号对应的属性行。
-//	 * @return 对应的属性行的键是否在至少一个相关的语言中不拥有。
-//	 * @throws IndexOutOfBoundsException 序号越界。
-//	 */
-//	public default boolean nonComplete(int index){
-//		return ! isComplete(index);
-//	}
-//	
-//	/**
-//	 * 
-//	 * @param index
-//	 * @return
-//	 */
-//	public Set<Locale> getIncompleteLocales(int index);
+	/**
+	 * 指定的属性行的键是否在所有相关的语言中都拥有。
+	 * @param index 指定的序号对应的属性行。
+	 * @return 对应的属性行的键是否在所有相关的语言中都拥有
+	 * @throws IndexOutOfBoundsException 序号越界。 
+	 */
+	public boolean isComplete(int index);
+	
+	/**
+	 * 指定的属性行的键是否在至少一个相关的语言中不拥有。
+	 * @param index 指定的序号对应的属性行。
+	 * @return 对应的属性行的键是否在至少一个相关的语言中不拥有。
+	 * @throws IndexOutOfBoundsException 序号越界。
+	 */
+	public default boolean nonComplete(int index){
+		return ! isComplete(index);
+	}
+	
+	/**
+	 * 
+	 * @param index
+	 * @return
+	 */
+	public Set<Locale> getIncompleteLocales(int index);
 
 }
